3bcdf244f01558bd0ac3ba6f03f65eb3906dfc6f
Oleh Hubskiy <gubskyolegod@gmail.com>
2023-11-13 15:14:35 +0200

    add nanoid

--- js/addTask.js
+++ js/addTask.js
@@ -1,6 +1,8 @@
+import { nanoid } from '../node_modules/nanoid/nanoid.js';
+
 export const addTask = (title, text, isImpotant) => {
   const newTask = {
-    id: Date.now(),
+    id: nanoid(),
     title,
     text,
     isDone: false,
